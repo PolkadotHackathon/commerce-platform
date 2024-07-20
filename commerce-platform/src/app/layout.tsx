c42af8c8845b5611aaa42a9e4838c27f489488b2
Toby Davis <pencilcaseman@gmail.com>
2024-07-20 23:05:05 +0100

    Tingly Tabs

--- commerce-platform/src/app/layout.tsx
+++ commerce-platform/src/app/layout.tsx
@@ -14,180 +14,180 @@ import { web3Accounts, web3Enable } from "@polkadot/extension-dapp";
 import { InjectedAccountWithMeta } from "@polkadot/extension-inject/types";
 
 interface LayoutProps {
-  children: ReactNode;
+    children: ReactNode;
 }
 
 const NAME = "BuyBuy";
 
 const Layout = ({ children }: LayoutProps) => {
-  const [api, setApi] = useState<ApiPromise | null>(null);
-  const [keyring, setKeyring] = useState<Keyring | null>(null);
-  const [accounts, setAccounts] = useState<InjectedAccountWithMeta[]>([]);
-  const [selectedAccount, setSelectedAccount] = useState<InjectedAccountWithMeta | null>(null);
-  const [showCookieConsent, setShowCookieConsent] = useState(true); // State for managing the cookie consent modal
-  const [isLoadingWeb3, setIsLoadingWeb3] = useState(false);
-  const [showAccountSelection, setShowAccountSelection] = useState(false);
+    const [api, setApi] = useState<ApiPromise | null>(null);
+    const [keyring, setKeyring] = useState<Keyring | null>(null);
+    const [accounts, setAccounts] = useState<InjectedAccountWithMeta[]>([]);
+    const [selectedAccount, setSelectedAccount] = useState<InjectedAccountWithMeta | null>(null);
+    const [showCookieConsent, setShowCookieConsent] = useState(true); // State for managing the cookie consent modal
+    const [isLoadingWeb3, setIsLoadingWeb3] = useState(false);
+    const [showAccountSelection, setShowAccountSelection] = useState(false);
 
-  const setup = async () => {
-    const provider = new WsProvider("ws://localhost:9944");
-    const api: ApiPromise = await ApiPromise.create({ provider });
-    setApi(api);
-    setKeyring(new Keyring({ type: "sr25519" }));
-  };
-
-  const handleConnection = async () => {
-    setIsLoadingWeb3(true);
-    const extensions = await web3Enable(NAME);
-
-    if (!extensions.length) {
-      setIsLoadingWeb3(false);
-      throw new Error("No extension found");
-    }
-
-    const allAccounts = await web3Accounts();
-    setAccounts(allAccounts);
-
-    if (allAccounts.length === 1) {
-      setSelectedAccount(allAccounts[0]);
-    } else if (allAccounts.length > 1) {
-      setShowAccountSelection(true); // Show account selection modal if more than one account is available
-    }
-    setIsLoadingWeb3(false);
-  };
-
-  const handleAccountSelection = (address) => {
-    const account = accounts.find((account) => account.address === address);
-    if (!account) {
-      throw new Error("Account not found");
-    }
-    setSelectedAccount(account);
-    setShowAccountSelection(false); // Hide the account selection modal
-  };
-
-  const handleAcceptCookies = () => {
-    handleConnection();
-    setShowCookieConsent(false);
-  };
-
-  const handleDeclineCookies = () => {
-    setShowCookieConsent(false);
-  };
-
-  useEffect(() => {
-    setup();
-  }, []);
-
-  useEffect(() => {
-    const handlePress = (event: any) => {
-      const target = event.target;
-      if (!target) return;
-
-      const id = target.id;
-      if (!id) return;
-
-      // Pad string to 64 bytes
-      const padded = id.padEnd(47, " ");
-
-      if (padded.length !== 47) {
-        console.error("ID is more than 47 characters, unable to record telemetry.");
-        return;
-      }
-
-      // Encrypt
-      const encrypted = CryptoJS.AES.encrypt(padded, NAME);
-      const encryptedBytes = CryptoJS.enc.Base64.parse(encrypted.toString());
-      const encryptedArray = Array.from(encryptedBytes.words);
-
-      // Decrypt
-      // const bytes = CryptoJS.AES.decrypt(encrypted, SECRET_KEY);
-      // const decrypted = bytes.toString(CryptoJS.enc.Utf8);
-      // console.log('Decrypted:', decrypted);
+    const setup = async () => {
+        const provider = new WsProvider("ws://localhost:9944");
+        const api: ApiPromise = await ApiPromise.create({ provider });
+        setApi(api);
+        setKeyring(new Keyring({ type: "sr25519" }));
     };
 
-    // Add event listeners
-    document.addEventListener("mousedown", handlePress);
-    document.addEventListener("touchstart", handlePress, { passive: false });
+    const handleConnection = async () => {
+        setIsLoadingWeb3(true);
+        const extensions = await web3Enable(NAME);
 
-    // Cleanup function to remove event listeners
-    return () => {
-      document.removeEventListener("mousedown", handlePress);
-      document.removeEventListener("touchstart", handlePress);
+        if (!extensions.length) {
+            setIsLoadingWeb3(false);
+            throw new Error("No extension found");
+        }
+
+        const allAccounts = await web3Accounts();
+        setAccounts(allAccounts);
+
+        if (allAccounts.length === 1) {
+            setSelectedAccount(allAccounts[0]);
+        } else if (allAccounts.length > 1) {
+            setShowAccountSelection(true); // Show account selection modal if more than one account is available
+        }
+        setIsLoadingWeb3(false);
     };
-  }, []); // Empty dependency array means this effect runs once on mount
 
-  return (
-      <CategoryProvider>
-        <CartProvider>
-          <html lang="en">
-          <head>
-            <meta charSet="UTF-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <title>BuyBuy</title>
-          </head>
-          <body>
-          <div style={styles.container}>
-            <CustomNavbar />
-            <div style={styles.main}>
-              <Sidebar />
-              <div style={styles.content}>{children}</div>
-            </div>
-          </div>
-          {isLoadingWeb3 && <div style={styles.blurBg}>Loading Web3...</div>}
-          {showCookieConsent && (
-              <CookieConsent onAccept={handleAcceptCookies} onDecline={handleDeclineCookies} />
-          )}
-          {showAccountSelection && (
-              <AccountSelectionModal
-                  accounts={accounts}
-                  onSelectAccount={handleAccountSelection}
-              />
-          )}
-          {/* Selected account picker */}
-          {selectedAccount == null && accounts.length > 1 && <></>}
-          </body>
-          </html>
-        </CartProvider>
-      </CategoryProvider>
-  );
+    const handleAccountSelection = (address) => {
+        const account = accounts.find((account) => account.address === address);
+        if (!account) {
+            throw new Error("Account not found");
+        }
+        setSelectedAccount(account);
+        setShowAccountSelection(false); // Hide the account selection modal
+    };
+
+    const handleAcceptCookies = () => {
+        handleConnection();
+        setShowCookieConsent(false);
+    };
+
+    const handleDeclineCookies = () => {
+        setShowCookieConsent(false);
+    };
+
+    useEffect(() => {
+        setup();
+    }, []);
+
+    useEffect(() => {
+        const handlePress = (event: any) => {
+            const target = event.target;
+            if (!target) return;
+
+            const id = target.id;
+            if (!id) return;
+
+            // Pad string to 64 bytes
+            const padded = id.padEnd(47, " ");
+
+            if (padded.length !== 47) {
+                console.error("ID is more than 47 characters, unable to record telemetry.");
+                return;
+            }
+
+            // Encrypt
+            const encrypted = CryptoJS.AES.encrypt(padded, NAME);
+            const encryptedBytes = CryptoJS.enc.Base64.parse(encrypted.toString());
+            const encryptedArray = Array.from(encryptedBytes.words);
+
+            // Decrypt
+            // const bytes = CryptoJS.AES.decrypt(encrypted, SECRET_KEY);
+            // const decrypted = bytes.toString(CryptoJS.enc.Utf8);
+            // console.log('Decrypted:', decrypted);
+        };
+
+        // Add event listeners
+        document.addEventListener("mousedown", handlePress);
+        document.addEventListener("touchstart", handlePress, { passive: false });
+
+        // Cleanup function to remove event listeners
+        return () => {
+            document.removeEventListener("mousedown", handlePress);
+            document.removeEventListener("touchstart", handlePress);
+        };
+    }, []); // Empty dependency array means this effect runs once on mount
+
+    return (
+        <CategoryProvider>
+            <CartProvider>
+                <html lang="en">
+                    <head>
+                        <meta charSet="UTF-8" />
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                        <title>BuyBuy</title>
+                    </head>
+                    <body>
+                        <div style={styles.container}>
+                            <CustomNavbar />
+                            <div style={styles.main}>
+                                <Sidebar />
+                                <div style={styles.content}>{children}</div>
+                            </div>
+                        </div>
+                        {isLoadingWeb3 && <div style={styles.blurBg}>Loading Web3...</div>}
+                        {showCookieConsent && (
+                            <CookieConsent onAccept={handleAcceptCookies} onDecline={handleDeclineCookies} />
+                        )}
+                        {showAccountSelection && (
+                            <AccountSelectionModal
+                                accounts={accounts}
+                                onSelectAccount={handleAccountSelection}
+                            />
+                        )}
+                        {/* Selected account picker */}
+                        {selectedAccount == null && accounts.length > 1 && <></>}
+                    </body>
+                </html>
+            </CartProvider>
+        </CategoryProvider>
+    );
 };
 
 const styles = {
-  body: {
-    margin: 0,
-    padding: 0,
-    height: "100vh",
-    overflow: "hidden",
-  },
-  container: {
-    fontFamily: "Arial, sans-serif",
-    display: "flex",
-    flexDirection: "column",
-    height: "100vh",
-  },
-  main: {
-    display: "flex",
-    flex: 1,
-    overflow: "hidden",
-  },
-  content: {
-    padding: "1rem",
-    flex: 1,
-    overflow: "hidden",
-  },
-  blurBg: {
-    fontFamily: "Arial, sans-serif",
-    position: "fixed",
-    top: 0,
-    left: 0,
-    width: "100%",
-    height: "100%",
-    backgroundColor: "rgba(255, 255, 255, 0.8)",
-    backdropFilter: "blur(10px)",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    zIndex: 1000,
-  },
+    body: {
+        margin: 0,
+        padding: 0,
+        height: "100vh",
+        overflow: "hidden",
+    },
+    container: {
+        fontFamily: "Arial, sans-serif",
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+    },
+    main: {
+        display: "flex",
+        flex: 1,
+        overflow: "hidden",
+    },
+    content: {
+        padding: "1rem",
+        flex: 1,
+        overflow: "hidden",
+    },
+    blurBg: {
+        fontFamily: "Arial, sans-serif",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        backgroundColor: "rgba(255, 255, 255, 0.8)",
+        backdropFilter: "blur(10px)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        zIndex: 1000,
+    },
 };
 
 export default Layout;
