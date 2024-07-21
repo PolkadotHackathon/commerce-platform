c4e905352a9951032bce64777f70fdb399cc50d7
Cy4 <veschi@gmail.com>
2024-07-21 12:42:33 +0100

    lgjhgljh

--- commerce-platform/src/app/layout.tsx
+++ commerce-platform/src/app/layout.tsx
@@ -31,8 +31,8 @@ const Layout = ({ children }: LayoutProps) => {
 
     const setup = async () => {
         // WARNING: ENSURE CORRECT ENDPOINT IS USED
-        // const provider = new WsProvider("ws://localhost:9944");
-        const provider = new WsProvider("wss://wet-games-sneeze.loca.lt");
+        const provider = new WsProvider("ws://localhost:9944");
+        // const provider = new WsProvider("wss://wet-games-sneeze.loca.lt");
         const api: ApiPromise = await ApiPromise.create({ provider });
         setApi(api);
         setKeyring(new Keyring({ type: "sr25519" }));
@@ -178,7 +178,7 @@ const Layout = ({ children }: LayoutProps) => {
                     <body>
                         <div style={styles.container}>
                             <CustomNavbar setShowAccountSelection={setShowAccountSelection} />
-                            {createButton()}
+                            {/* {createButton()} */}
                             <div style={styles.main}>
                                 <Sidebar />
                                 <div style={styles.content}>{children}</div>
@@ -214,24 +214,25 @@ const styles: Styles = {
     body: {
         margin: 0,
         padding: 0,
-        height: "100vh",
-        overflow: "hidden",
+        // height: "100vh",
+        overflow: "scroll",
     },
     container: {
         fontFamily: "Arial, sans-serif",
         display: "flex",
         flexDirection: "column",
         height: "100vh",
+        // paddingBottom: "3rem",
     },
     main: {
         display: "flex",
         flex: 1,
         overflow: "hidden",
+        paddingBottom: "3rem",
     },
     content: {
         padding: "1rem",
         flex: 1,
-        overflow: "hidden",
     },
     blurBg: {
         fontFamily: "Arial, sans-serif",
